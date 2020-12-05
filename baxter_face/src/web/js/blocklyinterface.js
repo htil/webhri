f22eacd58f3d586154fdb5ed4da847a7d8c2d920
Chris Crawford <chris35469@gmail.com>
2020-12-05 12:17:40 -0600

    Add custom pan_by block.

---
+++ baxter_face/src/web/js/blocklyinterface.js
@@ -0,0 +1,133 @@
+var createCustomBlocks = function(){
+    var mathChangeJson = {
+        "message0": "pan by %1",
+        "args0": [
+          {"type": "input_value", "name": "ANGLE", "check": "Number"}
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 355
+      };
+      
+      Blockly.Blocks['pan_by'] = {
+        init: function() {
+          this.jsonInit(mathChangeJson);
+        }
+    };
+
+    Blockly.JavaScript['pan_by'] = function(block) {
+        var angle = Blockly.JavaScript.valueToCode(block, 'ANGLE',
+        Blockly.JavaScript.ORDER_NONE)
+
+        angle = angle > 1.5 ? 1.5 : angle
+        angle = angle < -1.5 ? -1.5 : angle
+
+        code = `pan(${angle})`
+        return code;
+    }
+}
+
+
+
+
+
+/** Class used to manage Blockly interface
+ * @class
+*/
+
+
+var BlocklyInterface = function(){
+
+    // Handle custom block creation
+    createCustomBlocks()
+
+    // UI Elements
+    this.runButton = document.getElementById('runButton');
+
+    window.interpreter = null
+    window.runner = null
+    window.latestCode = ''
+    
+
+    window.workspace = Blockly.inject('blocklyDiv',
+    {media: 'https://unpkg.com/blockly/media/',
+     toolbox: document.getElementById('toolbox')});
+
+    Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),
+    window.workspace);
+
+    // Exit is used to signal the end of a script.
+    Blockly.JavaScript.addReservedWords('exit');
+
+    // Clear interpreter
+    window.resetInterpreter = function() {
+        window.interpreter = null;
+        if (window.runner) {
+            clearTimeout(window.runner)
+            window.runner = null  
+        }
+    }
+
+    window.generateCodeAndLoadIntoInterpreter = function() {
+        //Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
+        //Blockly.JavaScript.addReservedWords('highlightBlock');
+        window.latestCode = Blockly.JavaScript.workspaceToCode(window.workspace);
+        this.runButton = ''
+    }
+
+    window.initApi = function(interpreter, globalObject) {
+
+        // Create custom rappers 
+        var wrapper = function(cmd) {
+            console.log("wrapper: ", cmd)
+            window.pan(cmd)
+        }
+
+        interpreter.setProperty(globalObject, 'pan', 
+            interpreter.createNativeFunction(wrapper));
+
+
+        // Add an API for the wait block.  See wait_block.js
+        initInterpreterWaitForSeconds(interpreter, globalObject);
+    }
+
+    window.runBlocklyCode = function() {
+        console.log("latest Code: ", window.latestCode)
+        window.interpreter = new Interpreter(window.latestCode, window.initApi);
+        window.runner = function() {
+            var hasMore =  window.interpreter.run();
+            console.log("hasMore: ", hasMore)
+            if (hasMore) {
+                setTimeout(window.runner, 10);
+            } else {
+                console.log("window.resetInterpreter")
+                window.resetInterpreter()
+            }
+        }
+        window.runner()
+    }
+
+
+}
+
+BlocklyInterface.prototype.init = function() {
+    console.log("Blockly interface started")
+    window.generateCodeAndLoadIntoInterpreter()
+    window.workspace.addChangeListener(function(event) {
+        if (!(event instanceof Blockly.Events.Ui)) {
+          // Something changed. Parser needs to be reloaded.
+          window.resetInterpreter();
+          window.generateCodeAndLoadIntoInterpreter();
+          window.runBlockCode()
+        }
+    });
+}
+
+window.runBlockCode = function() {
+   if (!window.interpreter){
+       this.runButton = 'disabled'
+       console.log("setTimeout(window.runBlocklyCode, 1)")
+       setTimeout(window.runBlocklyCode, 1)
+   }
+}
+
--- baxter_face/src/web/js/main.js
+++ baxter_face/src/web/js/main.js
@@ -2,13 +2,30 @@
  * @class
 */
 var Main = function() {
-    window.ros = new ROS()
-    this.baxter = new Baxter()
 
+    //ROS
+    window.ros = new ROS()
+
+    //Baxter
+    this.baxter = new Baxter()
+    window.pan = this.baxter.updateHeadPan
+
+    this.blocklyInterface = new BlocklyInterface()
+    this.runButton = document.getElementById('runButton');
+
+    this.createEventListener = function(id, callback) {
+        document.getElementById(id).onclick = callback;
+    }
+
+    this.handleEvents = function(){
+        this.createEventListener("runButton", window.runBlockCode)
+    }
 }
 
 Main.prototype.start = function() {
     window.ros.init()
+    this.blocklyInterface.init()
+    this.handleEvents()
     this.baxter.updateFace("Happy", "SW", "White")
     this.baxter.updateHeadPan(0.0)
 }
